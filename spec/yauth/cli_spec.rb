f262d61414d9c94d4e6655b1a3048e78c3f83500
Matteo Collina <matteo.collina@gmail.com>
2011-02-24 01:00:24 +0100

    Added CLI spec.

--- spec/yauth/cli_spec.rb
+++ spec/yauth/cli_spec.rb
@@ -1,4 +1,46 @@
 require File.join(File.dirname(__FILE__), "..", "spec_helper")
+require 'fileutils'
 
 describe CLI do
+
+  subject { CLI.new }
+
+  before :each do
+    FileUtils.stub!(:mkdir_p)
+    @options = {}
+    subject.stub!(:options).and_return(@options)
+  end
+
+  [:add, :rm].each do |m|
+    it { should respond_to(m) }
+  end
+
+  it "should add a user to the manager with the specified config" do
+    @options[:config] = "config/users.yml"
+    manager = mock "Manager"
+    UserManager.should_receive(:load).with("config/users.yml").and_return(manager)
+
+    user = mock "User"
+    User.should_receive(:new).and_return(user)
+    user.should_receive(:username=).with("bar")
+    user.should_receive(:plain_password=).with("foo")
+
+    manager.should_receive(:add).with(user)
+
+    FileUtils.should_receive(:mkdir_p).with("config")
+    manager.should_receive(:save).with("config/users.yml")
+    subject.add("bar", "foo")
+  end
+
+  it "should remove from the manager with the specified config" do
+    @options[:config] = "config/users.yml"
+    manager = mock "Manager"
+    UserManager.should_receive(:load).with("config/users.yml").and_return(manager)
+
+    manager.should_receive(:remove).with("bar")
+
+    FileUtils.should_receive(:mkdir_p).with("config")
+    manager.should_receive(:save).with("config/users.yml")
+    subject.rm("bar")
+  end
 end
