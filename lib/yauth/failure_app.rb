66148ab28869806c789a2e026752551529967684
Matteo Collina <matteo.collina@gmail.com>
2011-03-01 10:39:47 +0100

    Added realm configuration.

--- lib/yauth/failure_app.rb
+++ lib/yauth/failure_app.rb
@@ -1,9 +1,15 @@
 
 class Yauth::FailureApp
+  attr_accessor :realm
+
+  def initialize(realm="Yauth secured area")
+    self.realm = realm
+  end
+
   def call(env)
     [401, 
       { "Content-Type" => "text/plain", 
-        "WWW-Authenticate" => %(Basic realm="Yauth secured area") }, 
+        "WWW-Authenticate" => %(Basic realm="#{realm}") }, 
       ["#{env['warden'].message}\n"]]
   end
 end
--- spec/yauth/failure_app_spec.rb
+++ spec/yauth/failure_app_spec.rb
@@ -25,4 +25,17 @@ describe FailureApp do
   it "should have the body set as the warden message" do
     subject.call(mock_env("hello world"))[2][0].should =~ /hello world/
   end
+
+  it { should respond_to(:realm) } 
+
+  it { should respond_to(:realm=) }
+
+  it "should have a default realm" do
+    subject.realm.should == "Yauth secured area"
+  end
+
+  it "should have WWW-Authenticate with the realm configured" do
+    subject.realm = "My Realm"
+    subject.call(mock_env)[1]["WWW-Authenticate"].should == %(Basic realm="My Realm")
+  end
 end
